da6b2803c48d6041552d7a46942a24c15e3ff112
ArtemEmelyanov <artem_emelyanov_2003@list.ru>
2023-08-23 19:20:33 +0400

    Fix: include Lombok, delete getter and setter in model User

--- src/main/java/com/example/demo/Model/User.java
+++ src/main/java/com/example/demo/Model/User.java
@@ -3,9 +3,15 @@ package com.example.demo.Model;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "_user")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,49 +24,10 @@ public class User {
     @Nullable
     private byte photo;
 
-    public User() {
-    }
-
     public User(String username, String login, String password, byte photo) {
         this.username = username;
         this.login = login;
         this.password = password;
-        this.photo = photo;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public byte getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(byte photo) {
         this.photo = photo;
     }
 }
