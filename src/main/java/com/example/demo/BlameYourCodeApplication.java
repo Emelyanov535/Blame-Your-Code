5ff84ed4d9887ceabbd5f7657690001d012b191d
ArtemEmelyanov <artem_emelyanov_2003@list.ru>
2023-08-31 22:18:45 +0400

    Fix: make war file

--- src/main/java/com/example/demo/BlameYourCodeApplication.java
+++ src/main/java/com/example/demo/BlameYourCodeApplication.java
@@ -2,9 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BlameYourCodeApplication {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(BlameYourCodeApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlameYourCodeApplication.class, args);
