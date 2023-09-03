d7110b7377b5996d6d91584e30714dd0b532031d
ArtemEmelyanov <artem_emelyanov_2003@list.ru>
2023-09-03 19:14:49 +0400

    Fix: delete unnecessary sql query

--- src/main/java/com/example/demo/Repository/IUserRepository.java
+++ src/main/java/com/example/demo/Repository/IUserRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface IUserRepository extends JpaRepository<User, Long> {
-
-    @Query("SELECT u FROM User u WHERE u.login = :login")
     User findByLogin(String login);
 }
